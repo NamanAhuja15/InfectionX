070c14f0c7be08ba9425c7fa6288be2d9238ce6a
NamanAhuja15 <46511996+NamanAhuja15@users.noreply.github.com>
2020-04-27 21:17:28 +0530

    Update

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     public GameObject Player;
@@ -13,16 +14,42 @@ public class UIManager : MonoBehaviour
     private Shooting ammo;
     void Start()
     {
-        health_script = Player.GetComponent<Player_Health>();
-        ammo = Player.GetComponent<Shooting>();
+        if (SceneManager.GetActiveScene().name == "2DGAME")
+        {
+            health_script = Player.GetComponent<Player_Health>();
+            ammo = Player.GetComponent<Shooting>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        bullet_count.text = ammo.Bullets.ToString();
-        rocket_count.text = ammo.Missiles.ToString();
-        Healthbar.value = health_script.health/100f;
-        health.text = health_script.health.ToString() + "/100";
+        if (SceneManager.GetActiveScene().name == "2DGAME")
+        {
+            bullet_count.text = ammo.Bullets.ToString();
+            rocket_count.text = ammo.Missiles.ToString();
+            Healthbar.value = health_script.health / 100f;
+            health.text = health_script.health.ToString() + "/100";
+        }
+    }
+    public void Story()
+    {
+        SceneManager.LoadScene("Story");
+    }
+     public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+     public void StartGame()
+    {
+        SceneManager.LoadScene("2DGAME");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("Start");
     }
 }
